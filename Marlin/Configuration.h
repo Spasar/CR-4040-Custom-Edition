1c906d3d8790a27bf506fa5f22f9c3967ab277c6
Spasar <spacequasar42@gmail.com>
2026-03-02 17:20:55 +0300

    Switched to the older MKS board

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -88,7 +88,7 @@
 
 // Choose the name from boards.h that matches your setup
 #ifndef MOTHERBOARD
-  #define MOTHERBOARD BOARD_MKS_ROBIN_NANO_V3_1
+  #define MOTHERBOARD BOARD_MKS_ROBIN_NANO
 #endif
 
 // @section serial
@@ -101,7 +101,7 @@
  *
  * :[-1, 0, 1, 2, 3, 4, 5, 6, 7]
  */
-#define SERIAL_PORT 0
+#define SERIAL_PORT -1
 
 /**
  * Serial Port Baud Rate
